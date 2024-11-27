77fea103a6adccdcf6051cf1c5344a62b371ad71
EduuGit <sosaflorese@gmail.com>
2024-11-27 02:26:24 -0300

    ,

--- My.Proyect/BaseDeDatosDocument.py
+++ My.Proyect/BaseDeDatosDocument.py
@@ -1,21 +1,17 @@
-from document import Document
 from collection import Collection
 
 class BaseDeDatosDocumental:
     def __init__(self):
         self.colecciones = {}
-    
-    def create_collection(self, nombre_coleccion):
+    def create_collection (self, nombre_coleccion):
         if nombre_coleccion not in self.colecciones:
             self.colecciones[nombre_coleccion] = Collection(nombre_coleccion)
-    
-    def eliminar_coleccion(self, nombre_coleccion):
+    def eliminar_coleccion (self, nombre_coleccion):
         if nombre_coleccion in self.colecciones:
             del self.colecciones[nombre_coleccion]
             
-    def get_collection(self, nombre_coleccion):
+    def get_collection (self, nombre_coleccion):
         return self.colecciones.get(nombre_coleccion, None)
     
     def __str__(self):
-        return f"Base de datos documental con {len(self.colecciones)} colecciones" 
-    
+        return f"Base de datos documental con {len(self.colecciones)} colecciones"
